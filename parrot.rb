63ee75125f9fa3e46b9a677f66c2c21b067a2fb7
qaisaru@icloud.com <qaisaru@icloud.com>
2018-09-06 15:29:34 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,3 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
-The #parrot method should accept an argument of a string and both #puts out that string and return the string at the end of the method.
-
-The #parrot method should have a default argument of "Squawk!".
-
 def parrot(parrot = "Squawk!")
 puts parrot
 return parrot
